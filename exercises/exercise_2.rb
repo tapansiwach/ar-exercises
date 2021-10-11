368a390419d51edaf6d006b4ccc51d3e9c2bd0aa
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 13:46:29 -0400

    Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(annual_revenue: 350000)
+
+updatedValue = Store.find(1).annual_revenue
+puts "Revenue of first store has been update to #{updatedValue}"
