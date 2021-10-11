e69fbc1d9833c0b81fac39ca8f3a3ed447720b36
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 16:33:47 -0400

    Exercise 7-1: Add validations to models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {more_than_or_equal_to: 0}
+  validate :store_has_to_sell_something
+
+  def store_has_to_sell_something
+    if !mens_apparel && !womens_apparel
+      errors.add("Store has to sell something")
+    end
+  end
+  
 end
