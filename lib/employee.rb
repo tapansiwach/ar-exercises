51d54a2c2d4672906ca34b682193647949bc04e1
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-11 18:41:01 -0400

    create a password before an emploee record is created

--- lib/employee.rb
+++ lib/employee.rb
@@ -8,4 +8,12 @@ class Employee < ActiveRecord::Base
     less_than_or_equal_to: 200
   }
   validates :store, presence: true
+  before_create :generate_password
+
+  private
+  def generate_password
+    chars = [*"a".."z", *"A".."Z", *"0".."9"]
+    self.password = Array.new(8) { chars.sample }.join
+  end
+  
 end
